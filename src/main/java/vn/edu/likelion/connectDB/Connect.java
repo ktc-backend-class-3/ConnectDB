4dd11ad2dd42e85ad1e184124e66f7dfabc3502c
anttech123 <54166617+anttech123@users.noreply.github.com>
2024-07-18 14:28:39 +0700

    update

--- src/main/java/vn/edu/likelion/connectDB/Connect.java
+++ src/main/java/vn/edu/likelion/connectDB/Connect.java
@@ -5,10 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connect {
-    private String url = "jdbc:oracle:thin:@localhost:1521:xe";
-    private String user = "system";
-    private String pass = "123456";
+    private String url;
+    private String user;
+    private String pass;
     private Connection conn = null;
+
+    public Connect() {
+        url = "jdbc:oracle:thin:@localhost:1521:xe";
+        user = "system";
+        pass = "123456";
+    }
 
     public Connection openConnect() throws SQLException {
         conn = DriverManager.getConnection(url, user, pass);
